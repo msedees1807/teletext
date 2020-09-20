0f4701643f4970a83ce1310139c55032778221c6
Marc Edees <Marc@Marcs-MBP-4.lan>
2020-09-20 15:40:16 +0100

    step 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">
-
-    
-  </div>;
+  return (
+    <div className="App">
+      <div className={"titleContainer"}>
+        <div className={"bbcTitle"}>
+          <h1>B</h1>
+        </div>
+        <div className={"bbcTitle"}>
+          <h1>B</h1>
+        </div>
+        <div className={"bbcTitle"}>
+          <h1>C</h1>
+        </div>
+        <div className={"footballTitle"}>
+          <h1 className={"BBCheader"} style={{ color: "#0efe05" }}>
+            FOOTBALL
+          </h1>
+        </div>
+      </div>
+      <h2 style={{ color: "#30d91e" }}>PREMIERSHIP RESULTS/FIXTURES</h2>
+      <h2 style={{ color: "#22d7cf" }}>CHARLTON</h2>
+      <p>Robinson 16</p>
+      <p>Parker 32</p>
+      <p>Euell 61</p>
+    </div>
+  );
 }
 
 export default App;
