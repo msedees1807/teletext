ac3623b006c51400c27163110e436e91d0defb4f
Marc Edees <marc_edees@hotmail.co.uk>
2020-10-13 22:42:59 +0100

    Updates

--- precache-manifest.83f4515babd1db678aa4729e0a077722.js
+++ precache-manifest.83f4515babd1db678aa4729e0a077722.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9a6a1eb022304e1a401eacb54d3de871",
+    "revision": "920f88e69ca91f47e7cd370a53647f54",
     "url": "/teletext/index.html"
   },
   {
-    "revision": "e5bdcec0e32a1f5a7733",
-    "url": "/teletext/static/css/main.9d38d615.chunk.css"
+    "revision": "699e697cd65b5a5ecce5",
+    "url": "/teletext/static/css/main.63d26b58.chunk.css"
   },
   {
     "revision": "92f223b11a6505d2ab5f",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/teletext/static/js/2.43a9128e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e5bdcec0e32a1f5a7733",
+    "revision": "699e697cd65b5a5ecce5",
     "url": "/teletext/static/js/main.fd2aefd9.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/teletext/precache-manifest.4f015ad8be5fd56a2def78f48fa08de9.js"
+  "/teletext/precache-manifest.83f4515babd1db678aa4729e0a077722.js"
 );
 
 self.addEventListener('message', (event) => {
