e0b0f2ac2d7b96607af8bff97257f52e53379784
Marc Edees <marc_edees@hotmail.co.uk>
2020-10-13 21:46:38 +0100

    Updates

---
+++ precache-manifest.1f24de17d7c8b60a5a9e8162bb612b21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0582dc04e88c8179774da5191b47950",
+    "url": "/teletext/index.html"
+  },
+  {
+    "revision": "837682d3b77c62d31ff7",
+    "url": "/teletext/static/css/main.9d38d615.chunk.css"
+  },
+  {
+    "revision": "92f223b11a6505d2ab5f",
+    "url": "/teletext/static/js/2.43a9128e.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/teletext/static/js/2.43a9128e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "837682d3b77c62d31ff7",
+    "url": "/teletext/static/js/main.97e555e5.chunk.js"
+  },
+  {
+    "revision": "decedb6ed13f06cae503",
+    "url": "/teletext/static/js/runtime-main.3ae88e0d.js"
+  }
+]);
