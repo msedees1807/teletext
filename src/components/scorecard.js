aba41e90a8943b2a0b5b84136f284fd48953c5e1
Marc Edees <marc_edees@hotmail.co.uk>
2020-10-17 21:09:04 +0100

    deconstructed and removed FC

--- src/components/scorecard.js
+++ src/components/scorecard.js
@@ -1,19 +1,25 @@
 import React from "react";
 
 function scorecard(props) {
-  console.log(props);
+
+  const {homeTeam, awayTeam} = props.data;
+  const homeScore = props.data.score.fullTime.homeTeam;
+  const awayScore = props.data.score.fullTime.awayTeam;
 
   return (
     <div className="scoresGrid">
-      <div style={{ color: "#27f9f9" }}>{props.data.homeTeam.name}</div>
+      <div style={{ color: "#27f9f9" }}>      
+        {homeTeam.name.endsWith("FC") ? homeTeam.name.replace("FC", "") : homeTeam.name}</div>
       <div style={{ textAlign: "center" }}>
-        {props.data.score.fullTime.homeTeam}
+        {homeScore}
       </div>
       <div style={{ textAlign: "center" }}>-</div>
       <div style={{ textAlign: "center" }}>
-        {props.data.score.fullTime.awayTeam}
+       {awayScore}
       </div>
-      <div style={{ color: "#27f9f9" }}>{props.data.awayTeam.name}</div>
+      <div style={{ color: "#27f9f9" }}>
+      {awayTeam.name.endsWith("FC") ? awayTeam.name.replace("FC", "") : awayTeam.name}
+      </div>
 
       {/* <div>
         <p>Cole 12, 42</p>
